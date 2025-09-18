015de84fac2023fea10ea1db75d70e9026a31309
Oleksandr Martynov <alex@Oleksandrs-MacBook-Air.local>
2025-09-18 21:10:21 +0300

    domain event implementation

--- QuizStep.Infrastructure/Data/ApplicationContext.cs
+++ QuizStep.Infrastructure/Data/ApplicationContext.cs
@@ -1,13 +1,35 @@
+using MediatR;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using QuizStep.Core.Entities;
+using QuizStep.Core.Primitives;
 
 namespace QuizStep.Infrastructure.Data;
 
-public class ApplicationContext: IdentityDbContext<User>
+public class ApplicationContext : IdentityDbContext<User>
 {
-    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    private readonly IPublisher _publisher;
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options, IPublisher publisher) : base(options)
     {
-        
+        _publisher = publisher;
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        var events = ChangeTracker.Entries<Entity>()
+            .Select(e => e.Entity)
+            .Where(e => e.Events.Any())
+            .SelectMany(e => e.Events);
+
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        foreach (var e in events)
+        {
+            await _publisher.Publish(e, cancellationToken);
+        }
+
+        return result;
     }
 }
