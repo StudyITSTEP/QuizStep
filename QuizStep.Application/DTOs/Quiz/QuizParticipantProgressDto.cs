3b4e55971060dadbb6d55e13b5fb97f78b18ab14
NVKT <nevermindktab@gmail.com>
2025-10-02 20:38:57 +0300

    QuizHub

---
+++ QuizStep.Application/DTOs/Quiz/QuizParticipantProgressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizStep.Application.DTOs.Quiz
+{
+    public class QuizParticipantProgressDto
+    {
+        public string UserId { get; set; } = default!;
+        public string UserName { get; set; } = default!;
+        public int CurrentQuestion { get; set; }
+        public int TotalQuestions { get; set; }
+    }
+}
